1148ed317abf6083118f21103f9f9acd741bf022
Bharadwaj Srigiriraju <1695154+bharadwaj6@users.noreply.github.com>
2024-03-16 00:02:00 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -205,6 +205,8 @@
   # https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
 
+  services.x2goserver.enable = true;
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
