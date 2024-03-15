51abe790837eb233de2c040513905271c7570288
bharadwaj6 <1695154+bharadwaj6@users.noreply.github.com>
2024-03-16 01:08:16 +0530

    change dir structure

--- flake.nix
+++ flake.nix
@@ -4,18 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./configuration.nix
-        # inputs.home-manager.nixosModules.default
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
       ];
     };
   };
