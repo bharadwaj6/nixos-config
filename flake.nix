00e60dacb2c7134c1b9b604931d80c8011867ff6
bharadwaj6 <1695154+bharadwaj6@users.noreply.github.com>
2024-03-16 17:45:03 +0530

    remove microvm flake

--- flake.nix
+++ flake.nix
@@ -8,17 +8,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    microvm = {
-      url = "github:astro/microvm.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    microvm,
     ...
   } @ inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
@@ -26,29 +20,6 @@
       modules = [
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
-        inputs.microvm.nixosModules.microvm
-        {
-          users.users.root.password = "";
-          microvm = {
-                volumes = [ {
-                  mountPoint = "/var";
-                  image = "var.img";
-                  size = 256;
-                } ];
-                shares = [ {
-                  # use "virtiofs" for MicroVMs that are started by systemd
-                  proto = "9p";
-                  tag = "ro-store";
-                  # a host's /nix/store will be picked up so that no
-                  # squashfs/erofs will be built for it.
-                  source = "/nix/store";
-                  mountPoint = "/nix/.ro-store";
-                } ];
-
-                hypervisor = "qemu";
-                socket = "control.socket";
-              };
-        }
       ];
     };
   };
